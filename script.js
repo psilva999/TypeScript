5916968763b985086d3320181ec30c2ffb8d6551
PSILVA999 <p.silva0399@gmail.com>
2022-11-23 14:25:44 -0300

    annotation, inference, functions

--- script.js
+++ script.js
@@ -3,3 +3,60 @@ function multi(a, b) {
     return a + b;
 }
 console.log(multi('Hello,', 'World!'));
+//annotation
+const agenda = 'Cheia';
+const diasJaneiro = 31;
+const war = {
+    pais: 'EUA',
+    year: 1945,
+    enemy: 'Nazi'
+};
+//Inference
+const region = 'Omaha', day = 6, impor = 'dia D', year = 1944, month = 'June';
+console.log(`Na praia de ${region}, no dia 0${6}/${month}/${year}, os aliados iniciaram a ofensiva conhecida como ${impor}`);
+const ww2 = {
+    pais: 'EUA',
+    year: 1945,
+    enemy: 'Nazi'
+};
+const end = year === 1944 ? true : 'ano errado';
+//Function
+function berlin(a, b, c) {
+    return `${a}! ${b}! Berlin is ${c}!`;
+}
+console.log(berlin('Attero', 'Dominatus', 'burning'));
+const bandas = {
+    nome: 'sabaton',
+    origem: 'suecia',
+    formacao: '1999'
+};
+function saberDaBanda(banda) {
+    banda.origem = `país europeu: ${banda.origem}`;
+    return banda;
+}
+const talDeBanda = saberDaBanda(bandas);
+console.log(talDeBanda);
+//Exercício 1
+function ajustaTexto(txt) {
+    return txt.trim().toLowerCase();
+}
+console.log(ajustaTexto(' Empire of the Clouds '));
+//Exercício 2
+const input = document.querySelector("input"), total = localStorage.getItem('total');
+if (input && total) {
+    input.value = total;
+    totalGanho(+input.value);
+}
+function totalGanho(value) {
+    const p = document.querySelector('p');
+    if (p)
+        p.innerText = `total: ${value + 100 - value * .2}`;
+}
+function totalMudou() {
+    if (input) {
+        localStorage.setItem('total', input.value);
+        totalGanho(+input.value);
+    }
+}
+if (input)
+    input.addEventListener('keyup', totalMudou);
