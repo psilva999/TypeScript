27626d192e3cef47a1c97af0c38d249cc842defb
PSILVA999 <p.silva0399@gmail.com>
2022-11-23 09:03:35 -0300

    Hello, World!

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict";
+function multi(a, b) {
+    return a + b;
+}
+console.log(multi('Hello,', 'World!'));
---
+++ script.ts
@@ -0,0 +1,5 @@
+function multi(a: string, b: string) {
+  return a + b
+}
+
+console.log(multi('Hello,', 'World!'))
